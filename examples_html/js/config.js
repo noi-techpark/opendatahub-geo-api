028361e1b872ec2c17e0a475159d28d4a6ce8436
RudiThoeni <r.thoeni@noi.bz.it>
2026-02-12 21:33:09 +0100

    adding reuse fix

--- examples_html/js/config.js
+++ examples_html/js/config.js
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 const CONFIG = {
     apiUrl: 'https://geo.api.opendatahub.testingmachine.eu',
     //apiUrl: 'http://localhost:5023',
